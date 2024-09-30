3a1635773bb2672afea28ed7c0d6b54308270e69
Bilqees Shahid <161162316+BilqeesShahid@users.noreply.github.com>
2024-10-01 02:21:27 +0500

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,8 +8,8 @@ const Navbar = () => {
         <div className='flex w-fit gap-10 mx-auto font-medium py-4 text-blackish'>
           <Link className='navbar_link relative' href="#">Home</Link>
           <Link className='navbar_link relative' href="#">Categories</Link>
-          <Link className='navbar_link relative' href="#">Men's</Link>
-          <Link className='navbar_link relative' href="#">Women's</Link>
+          <Link className='navbar_link relative' href="#">Men</Link>
+          <Link className='navbar_link relative' href="#">Women</Link>
           <Link className='navbar_link relative' href="#">Accessories</Link>
           <Link className='navbar_link relative' href="#">Clothing</Link>
         </div>
